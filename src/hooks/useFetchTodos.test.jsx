eeb3d34fdb2df23a28a9ef7161e390cb012f857d
jgarnett <jgarnett18@gmail.com>
2024-02-10 14:17:22 -0500

    added useFetchTodos tests

---
+++ src/hooks/useFetchTodos.test.jsx
@@ -0,0 +1,97 @@
+import {cleanup, render, waitFor} from '@testing-library/react';
+import {Provider} from 'react-redux';
+import useFetchTodos from './useFetchTodos';
+import {useEffect} from 'react';
+import setupStore from '../store/setupStore';
+import TodoApi from '../api/TodoApi';
+
+const TestComponent = () => {
+    const fetchTodos = useFetchTodos();
+    useEffect(() => {
+        fetchTodos();
+    }, []);
+    return (<div>&nbsp;</div>);
+};
+jest.mock('../api/TodoApi');
+
+describe('useFetchTodos', () => {
+    const mockStore = {
+        todoApi: {
+            filters: {
+                done: true
+            },
+            limit: 10,
+            sort: 'desc',
+            page: 1
+        }
+    };
+    let store;
+    beforeEach(() => {
+        mockStore.todoApi = {
+            filters: {
+                done: true
+            },
+            limit: 10,
+            sort: 'desc',
+            page: 1
+        };
+        store = setupStore(mockStore);
+    });
+    afterEach(cleanup);
+    const _render = () => render(
+        <Provider store={store}>
+            <TestComponent />
+        </Provider>
+    );
+    const mockResponse = {
+        todos: [],
+        totalCount: 0
+    };
+    TodoApi.getTodos.mockResolvedValue(mockResponse);
+    it('should call TodoApi with expected options', () => {
+        _render();
+        expect(TodoApi.getTodos).toHaveBeenCalledWith({
+            done: true,
+            _limit: 10,
+            _order: 'desc',
+            _sort: 'date',
+            _page: 1
+        });
+    });
+    it('should set loading on store', async () => {
+        const spy = jest.spyOn(store, 'dispatch');
+        _render();
+        await waitFor(() => {
+            expect(spy.mock.calls).toContainEqual([{ type: 'LOADING', value: true }]);
+            expect(spy.mock.calls).toContainEqual([{ type: 'LOADING', value: false }]);
+        });
+    });
+    it('should set todos on store', async () => {
+        const spy = jest.spyOn(store, 'dispatch');
+        _render();
+        await waitFor(() => {
+            expect(spy).toHaveBeenCalledWith({ type: 'SET', todos: [] });
+        });
+    });
+    it('should set total pages on store', async () => {
+        const spy = jest.spyOn(store, 'dispatch');
+        _render();
+        await waitFor(() => {
+            expect(spy.mock.calls).toContainEqual([{ type: 'TOTAL', value: 1 }]);
+        });
+    });
+    it('should set reset page if total pages changes', async () => {
+        store = setupStore({
+            todoApi: {
+                ...mockStore.todoApi,
+                page: 3
+            }
+        });
+        const spy = jest.spyOn(store, 'dispatch');
+        mockResponse.totalCount = 20;
+        _render();
+        await waitFor(() => {
+            expect(spy.mock.calls).toContainEqual([{ type: 'PAGE', value: 2 }]);
+        });
+    });
+});
